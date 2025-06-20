b8875231201b6331fbd5e8cd4846de2fe0a53552
lepri4dw <lepri4dw@gmail.com>
2025-06-20 17:56:22 +0600

    Added redirects file

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
-export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || 'your_client_id';
+export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || '1038543715700-9oggqokk5brg967n2g04lh1om7ukf7dm.apps.googleusercontent.com';
